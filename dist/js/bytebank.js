9c42fb31ea17b32d28818d6fe9e1e407b306d6f1
ericknovais <erick.hora@outlook.com>
2024-11-29 01:32:33 -0300

    Alteração da estrutura do projeto

--- dist/js/bytebank.js
+++ dist/js/bytebank.js
@@ -11,12 +11,12 @@ elementoFormulario.addEventListener('submit', function (event) {
         return;
     }
     ;
-    var inputTipoTransacao = elementoFormulario.querySelector("#tipoTransacao");
-    var inputValor = elementoFormulario.querySelector("#valor");
-    var inputData = elementoFormulario.querySelector("#data");
-    var tipoTransacao = inputTipoTransacao.value;
-    var valor = inputValor.valueAsNumber;
-    var data = new Date(inputData.value);
+    const inputTipoTransacao = elementoFormulario.querySelector("#tipoTransacao");
+    const inputValor = elementoFormulario.querySelector("#valor");
+    const inputData = elementoFormulario.querySelector("#data");
+    let tipoTransacao = inputTipoTransacao.value;
+    let valor = inputValor.valueAsNumber;
+    let data = new Date(inputData.value);
     if (tipoTransacao == "Depósito") {
         saldo += valor;
     }
@@ -28,7 +28,7 @@ elementoFormulario.addEventListener('submit', function (event) {
         return;
     }
     elementoSaldo.textContent = saldo.toString();
-    var novaTransacao = {
+    const novaTransacao = {
         tipoTransacao: tipoTransacao,
         valor: valor,
         data: data
